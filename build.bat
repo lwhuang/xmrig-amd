7cfc029aec9b923386777aee45b88f974ef81f19
liwu.huang <liwu.huang@latticeworkinc.com>
2017-11-14 18:03:50 +0800

    update path

--- build.bat
+++ build.bat
@@ -1,1 +1,14 @@
-cmake -DUV_LIBRARY=%project%\libuv\Release\lib\libuv.lib -DUV_INCLUDE_DIR=%project%\libuv\include -DOpenCL_LIBRARY=%PROJECT%\OCL_SDK_Light\lib\x86_64\opencl.lib -DOpenCL_INCLUDE_DIR=%PROJECT%\OCL_SDK_Light\include -DMHD_INCLUDE_DIR=%PROJECT%\libmicrohttpd-0.9.55-w32-bin\x86_64\VS2015\Release-static -DMHD_LIBRARY=%PROJECT%\libmicrohttpd-0.9.55-w32-bin\x86_64\VS2015\Release-static\libmicrohttpd.lib -G "Visual Studio 14 2015 Win64" ..
+SET rootpath=%~dp0
+
+cd src\3rdparty\libuv
+call vcbuild release x64
+
+
+cd %rootpath%
+set bdir=buildx64
+rmdir /s /q %bdir%
+mkdir %bdir%
+cd %bdir%
+
+
+cmake -DUV_LIBRARY=%rootpath%src\3rdparty\libuv\Release\lib\libuv.lib -DUV_INCLUDE_DIR=%rootpath%src\3rdparty\libuv\include -DOpenCL_LIBRARY=%rootpath%lib\OCL_SDK_Light\lib\x86_64\opencl.lib -DOpenCL_INCLUDE_DIR=%rootpath%lib\OCL_SDK_Light\include -DMHD_INCLUDE_DIR=%rootpath%lib\libmicrohttpd-0.9.55-w32-bin\x86_64\VS2015\Release-static -DMHD_LIBRARY=%rootpath%lib\libmicrohttpd-0.9.55-w32-bin\x86_64\VS2015\Release-static\libmicrohttpd.lib -G "Visual Studio 14 2015 Win64" ..
